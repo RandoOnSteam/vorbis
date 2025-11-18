ebdd74bcb4ec6849f450259c5e595a87ae9ac907
RandoOnSteam <battlemageloveryt@gmail.com>
2025-11-18 15:54:35 -0800

    def types

---
+++ include/ogg/config_types.h
@@ -0,0 +1,6 @@
+   typedef signed short ogg_int16_t;
+   typedef unsigned short ogg_uint16_t;
+   typedef signed long ogg_int32_t;
+   typedef unsigned long ogg_uint32_t;
+   typedef long long int ogg_int64_t;
+   typedef unsigned long long int ogg_uint64_t;
